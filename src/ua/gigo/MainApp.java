14422db7a972221ed39ee942404696888982284a
Taras <31373000+gigo123@users.noreply.github.com>
2022-01-16 11:38:44 -0500

    Create MainApp.java
    add English alphabet  score table

---
+++ src/ua/gigo/MainApp.java
@@ -0,0 +1,41 @@
+package ua.gigo;
+
+import java.util.HashMap;
+
+public class MainApp {
+	
+	private HashMap<String, Integer> getScoreTable(){
+		HashMap<String, Integer> score = new  HashMap<>();
+		score.put("a", 1);
+		score.put("b", 2);
+		score.put("c", 3);
+		score.put("d", 4);
+		score.put("e", 5);
+		score.put("f", 6);
+		score.put("g", 7);
+		score.put("h", 8);
+		score.put("i", 9);
+		score.put("j", 10);
+		score.put("k", 11);
+		score.put("l", 12);
+		score.put("m", 13);
+		score.put("n", 14);
+		score.put("o", 15);
+		score.put("p", 16);
+		score.put("q", 17);
+		score.put("r", 18);
+		score.put("s", 19);
+		score.put("t", 20);
+		score.put("u", 21);
+		score.put("v", 22);
+		score.put("w", 23);
+		score.put("x", 24);
+		score.put("y", 25);
+		score.put("z", 26);
+		return score;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
